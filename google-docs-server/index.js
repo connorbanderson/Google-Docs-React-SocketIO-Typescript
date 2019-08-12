14ba2fcd30ddab75147ffa10ce8e8fd91e38eb83
connorbanderson <connorbanderson@gmail.com>
2019-08-11 22:05:17 -0600

    Server - Initial Value Emit

--- google-docs-server/index.js
+++ google-docs-server/index.js
@@ -2,9 +2,31 @@ const app = require("express")();
 const http = require("http").createServer(app);
 const io = require("socket.io")(http);
 
+// Vulnerable to race conditions, not the point of this repo however
+// so this will do.
+
+let value = {
+  document: {
+    nodes: [
+      {
+        object: "block",
+        type: "paragraph",
+        nodes: [
+          {
+            object: "text",
+            text: "A line of text in a paragraph."
+          }
+        ]
+      }
+    ]
+  }
+};
+
 io.on("connection", function(socket) {
   console.log("a user conncented");
+  io.emit("init-value", value);
   socket.on("new-operations", function(data) {
+    value = data.value;
     io.emit("new-remote-operations", data);
   });
 });
